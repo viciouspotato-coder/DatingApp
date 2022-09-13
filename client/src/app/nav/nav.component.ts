add4b6e529c22bc0bc3fb4b9e58bcd20d991997c
Henry Tao <taohenry1999@gmail.com>
2022-09-12 21:13:51 -0400

    section 8

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,10 +21,6 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => this.rounter.navigateByUrl('/members'),
-      error: error => {
-        console.log(error);
-        this.toastr.error(error.error);
-      }
     })
   }
 
